1b04a79046c257b7808d26b3b1430bddaeadee23
Devota Aabel <accounts@devota.dev>
2024-03-06 22:30:57 -0500

    Added trending section with trending hashtags
    - Did some setup for trending links and trending statuses as well
    - TODO setup retry

--- feature/discover/src/main/java/social/firefly/feature/discover/DiscoverModule.kt
+++ feature/discover/src/main/java/social/firefly/feature/discover/DiscoverModule.kt
@@ -1,17 +1,17 @@
 package social.firefly.feature.discover
 
+import androidx.paging.ExperimentalPagingApi
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 import social.firefly.common.commonModule
 import social.firefly.core.analytics.analyticsModule
 import social.firefly.core.navigation.navigationModule
-import social.firefly.core.usecase.mozilla.mozillaUsecaseModule
 
+@OptIn(ExperimentalPagingApi::class)
 val discoverModule =
     module {
         includes(
             commonModule,
-            mozillaUsecaseModule,
             navigationModule,
             analyticsModule,
         )
