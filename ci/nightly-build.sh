85472b579ad7ee6ac1906343c0f63d9c1c86baaf
JohnOberhauser <14130581+JohnOberhauser@users.noreply.github.com>
2024-09-22 22:19:46 -0500

    fixing nightly build I hope (#105)
    Co-authored-by: John Oberhauser <j.git-global@obez.io>

--- ci/nightly-build.sh
+++ ci/nightly-build.sh
@@ -3,22 +3,16 @@
 # Fail if any commands fails.
 set -e
 
-APK_OUTPUT_DIR="app/build/outputs/apk/nightly"
-UNSIGNED_APK="$APK_OUTPUT_DIR/app-nightly-unsigned.apk"
-ALIGNED_APK="$APK_OUTPUT_DIR/nightly-aligned.apk"
-SIGNED_APK="$APK_OUTPUT_DIR/nightly.apk"
+SIGNED_APK="app/build/outputs/apk/nightly/app-nightly.apk"
 SIGNED_APK_FINAL_PATH="secrets/nightly.apk"
 
 BUILD_TOOLS_DIR="$ANDROID_HOME/build-tools/33.0.1"
 KEY_STORE="secrets/firefly.jks"
 
-if [[ ! -f "$SECRET_ENV" ]]; then
+if [[ ! -f "$KEY_STORE" ]]; then
   echo "Secrets not decrypted"
   exit 1
 fi
-
-echo "Initializing secrets…"
-source "secrets/secret-environment-variables.sh"
 
 echo "Building APK"
 ./gradlew :app:assembleNightly
