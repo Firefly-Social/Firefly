b7effcbdee4b889490f219e5fca22a40400641eb
John Oberhauser <j.git-global@obez.io>
2024-09-28 13:39:52 -0500

    fixing block quotes

--- core/ui/htmlcontent/src/main/kotlin/social/firefly/core/ui/htmlcontent/HtmlStringExtensions.kt
+++ core/ui/htmlcontent/src/main/kotlin/social/firefly/core/ui/htmlcontent/HtmlStringExtensions.kt
@@ -1,7 +1,11 @@
 package social.firefly.core.ui.htmlcontent
 
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.text.Layout
 import android.text.Spannable
 import android.text.TextPaint
+import android.text.style.QuoteSpan
 import android.text.style.URLSpan
 import android.view.View
 import androidx.compose.ui.graphics.Color
@@ -33,48 +37,119 @@ fun String.htmlToClickableSpannable(
 ): Spannable {
     return htmlToSpannable()
         .apply {
-            val urlSpans = getSpans(0, length, URLSpan::class.java)
-            urlSpans.forEach { span ->
-                val start = getSpanStart(span)
-                val end = getSpanEnd(span)
-                val flags = getSpanFlags(span)
-                val spanText = substring(start, end)
-                setSpan(
-                    object : URLSpan(span.url) {
-                        override fun onClick(view: View) {
-                            when {
-                                spanText.startsWith("@") -> {
-                                    // find the correct mention
-                                    mentions.find {
-                                        // check the user name and the domain
-                                        it.acct.substringBeforeLast("@") == spanText.substringAfter(
-                                            "@"
-                                        ) &&
-                                                url.contains(it.acct.substringAfter("@"))
-                                    }?.accountId?.let {
-                                        onAccountClicked(it)
-                                    }
-                                }
+            editUrlSpans(
+                mentions = mentions,
+                linkColor = linkColor,
+                onLinkClick = onLinkClick,
+                onHashTagClicked = onHashTagClicked,
+                onAccountClicked = onAccountClicked,
+            )
+            editBlockQuoteSpans(
+                color = linkColor,
+            )
+        }
+}
 
-                                spanText.startsWith("#") ->
-                                    onHashTagClicked(spanText.substringAfter("#"))
-
-                                else -> onLinkClick(url)
+fun Spannable.editUrlSpans(
+    mentions: List<Mention>,
+    linkColor: Color,
+    onLinkClick: (url: String) -> Unit,
+    onHashTagClicked: (hashTag: String) -> Unit,
+    onAccountClicked: (accountName: String) -> Unit,
+) {
+    val urlSpans = getSpans(0, length, URLSpan::class.java)
+    urlSpans.forEach { span ->
+        val start = getSpanStart(span)
+        val end = getSpanEnd(span)
+        val flags = getSpanFlags(span)
+        val spanText = substring(start, end)
+        setSpan(
+            object : URLSpan(span.url) {
+                override fun onClick(view: View) {
+                    when {
+                        spanText.startsWith("@") -> {
+                            // find the correct mention
+                            mentions.find {
+                                // check the user name and the domain
+                                it.acct.substringBeforeLast("@") == spanText.substringAfter(
+                                    "@"
+                                ) &&
+                                        url.contains(it.acct.substringAfter("@"))
+                            }?.accountId?.let {
+                                onAccountClicked(it)
                             }
                         }
 
-                        override fun updateDrawState(ds: TextPaint) {
-                            super.updateDrawState(ds)
-                            ds.color = linkColor.toArgb()
-                        }
-                    },
-                    start,
-                    end,
-                    flags,
-                )
-                removeSpan(span)
-            }
-        }
+                        spanText.startsWith("#") ->
+                            onHashTagClicked(spanText.substringAfter("#"))
+
+                        else -> onLinkClick(url)
+                    }
+                }
+
+                override fun updateDrawState(ds: TextPaint) {
+                    super.updateDrawState(ds)
+                    ds.color = linkColor.toArgb()
+                }
+            },
+            start,
+            end,
+            flags,
+        )
+        removeSpan(span)
+    }
+}
+
+fun Spannable.editBlockQuoteSpans(
+    color: Color,
+    stripeWidth: Int = 10,
+    gapWidth: Int = 50
+) {
+    val spans = getSpans(0, length, QuoteSpan::class.java)
+    spans.forEach { span ->
+        val start = getSpanStart(span)
+        val end = getSpanEnd(span)
+        val flags = getSpanFlags(span)
+        setSpan(
+            object : QuoteSpan() {
+                override fun getLeadingMargin(first: Boolean): Int {
+                    return stripeWidth + gapWidth
+                }
+
+                override fun drawLeadingMargin(
+                    c: Canvas,
+                    p: Paint,
+                    x: Int,
+                    dir: Int,
+                    top: Int,
+                    baseline: Int,
+                    bottom: Int,
+                    text: CharSequence,
+                    start: Int,
+                    end: Int,
+                    first: Boolean,
+                    layout: Layout
+                ) {
+                    val originalPaintColor = p.color
+                    val paint = p.apply { this.color = color.toArgb() }
+
+                    // Draw the stripe (quote line)
+                    c.drawRect(
+                        x.toFloat(),
+                        top.toFloat(),
+                        (x + dir * stripeWidth).toFloat(),
+                        bottom.toFloat(),
+                        paint
+                    )
+                    p.color = originalPaintColor
+                }
+            },
+            start,
+            end,
+            flags,
+        )
+        removeSpan(span)
+    }
 }
 
 @Suppress("MaxLineLength")
