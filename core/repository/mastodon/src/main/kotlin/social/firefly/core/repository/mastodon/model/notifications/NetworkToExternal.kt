656582171e95d5ab6c0838b45649e8267041a616
John Oberhauser <j.git-global@obez.io>
2025-12-31 13:25:18 -0600

    fixing detekt

--- core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/model/notifications/NetworkToExternal.kt
+++ core/repository/mastodon/src/main/kotlin/social/firefly/core/repository/mastodon/model/notifications/NetworkToExternal.kt
@@ -14,6 +14,7 @@ import social.firefly.core.network.mastodon.model.responseBody.NetworkNotificati
 import social.firefly.core.network.mastodon.model.responseBody.NetworkRelationshipSeveranceEvent
 import social.firefly.core.repository.mastodon.model.status.toExternalModel
 
+@Suppress("CyclomaticComplexMethod")
 fun NetworkNotification.toExternal(): Notification =
     when (this) {
         is NetworkNotification.Mention -> Notification.Mention(
